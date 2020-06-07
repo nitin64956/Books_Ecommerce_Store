b12ac1debc87b96f73cc012112f77c5205cd942c
nitin64956 <nitin64956@gmail.com>
2020-06-07 09:14:52 +0530

    Orders Changes

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -36,7 +36,7 @@ class orders extends Component {
       }
       
     render() {
-        let array = this.state.fetchedOrders.sort((a,b) => a.date >= b.date);
+        let array = this.state.fetchedOrders.reverse();
         
     return (
         <div className = "ordercontainer">
